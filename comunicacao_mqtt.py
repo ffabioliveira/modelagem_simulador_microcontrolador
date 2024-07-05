05e4d727194d4823a532a7e3ad5e292b878c98cf
ffabioliveira <ffabioliveira81@gmail.com>
2024-07-04 22:59:55 -0300

    Atualiza classe de comunicação via MQTT para suportar callbacks

--- comunicacao_mqtt.py
+++ comunicacao_mqtt.py
@@ -4,8 +4,11 @@ class ComunicacaoMQTT:
     def __init__(self, broker, client_id):
         self.client = mqtt.Client(client_id)
         self.broker = broker
+        self.on_message_callback = None
 
     def conectar(self):
+        self.client.on_connect = self.on_connect
+        self.client.on_message = self.on_message
         self.client.connect(self.broker)
         self.client.loop_start()
 
@@ -13,6 +16,19 @@ class ComunicacaoMQTT:
         self.client.publish(topico, mensagem)
 
     def inscrever(self, topico, callback):
+        self.on_message_callback = callback
         self.client.subscribe(topico)
         self.client.message_callback_add(topico, callback)
 
+    def on_connect(self, client, userdata, flags, rc):
+        if rc == 0:
+            print("Microcontrolador conectado ao broker!")
+            client.subscribe("borda/to/microcontrolador")
+            client.publish("microcontrolador/to/borda", "Microcontrolador conectado!")
+            print("Microcontrolador informou: Microcontrolador conectado!")
+        else:
+            print(f"Falha na conexão do Microcontrolador. Código de retorno: {rc}")
+
+    def on_message(self, client, userdata, message):
+        if self.on_message_callback:
+            self.on_message_callback(client, userdata, message)
